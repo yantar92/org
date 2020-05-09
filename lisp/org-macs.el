ee1770afde5107f89818449b3cc1fd1cbe4a7975
Ihor Radchenko <yantar92@gmail.com>
2020-05-09 22:43:36 +0800

    Remove unnecessary org-with-point-at from org--find-text-property-region

--- lisp/org-macs.el
+++ lisp/org-macs.el
@@ -707,20 +707,19 @@ If DELETE is non-nil, delete all those overlays."
 
 (defun org--find-text-property-region (pos prop)
   "Find a region containing PROP text property around point POS."
-  (org-with-point-at pos
-    (let* ((beg (and (get-text-property pos prop) pos))
-	   (end beg))
-      (when beg
-        ;; when beg is the first point in the region, `previous-single-property-change'
-        ;; will return nil.
-	(setq beg (or (previous-single-property-change pos prop)
-		      beg))
-        ;; when end is the last point in the region, `next-single-property-change'
-        ;; will return nil.
-	(setq end (or (next-single-property-change pos prop)
-		      end))
-        (unless (equal beg end) ; this should not happen
-          (cons beg end))))))
+  (let* ((beg (and (get-text-property pos prop) pos))
+	 (end beg))
+    (when beg
+      ;; when beg is the first point in the region, `previous-single-property-change'
+      ;; will return nil.
+      (setq beg (or (previous-single-property-change pos prop)
+		    beg))
+      ;; when end is the last point in the region, `next-single-property-change'
+      ;; will return nil.
+      (setq end (or (next-single-property-change pos prop)
+		    end))
+      (unless (equal beg end) ; this should not happen
+        (cons beg end)))))
 
 (defun org-flag-region (from to flag spec)
   "Hide or show lines from FROM to TO, according to FLAG.
