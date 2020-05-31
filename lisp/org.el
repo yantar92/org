0c3857e7658e6dbe8ac92f9537ef2e0e708878ab
Ihor Radchenko <yantar92@gmail.com>
2020-05-31 20:15:39 +0800

    Disable region-local modification-hooks

--- lisp/org.el
+++ lisp/org.el
@@ -6289,17 +6289,17 @@ Return a non-nil value when toggling is successful."
             ;; of blocks is practically more rare in comparison with
             ;; folding/unfolding. Removing modification hooks would
             ;; cost more CPU time.
-            (with-silent-modifications
-              (when (and flag
-			 (not (member #'org--unfold-elements-in-region
-				    (get-text-property (org-element-property :begin element)
-						       'modification-hooks))))
-		;; first line
-		(org--add-to-list-text-property (org-element-property :begin element) start
-					     'modification-hooks #'org--unfold-elements-in-region)
-		;; last line
-		(org--add-to-list-text-property (save-excursion (goto-char end) (line-beginning-position)) end
-					     'modification-hooks #'org--unfold-elements-in-region)))
+            ;; (with-silent-modifications
+            ;;   (when (and flag
+	    ;; 		 (not (member #'org--unfold-elements-in-region
+	    ;; 			    (get-text-property (org-element-property :begin element)
+	    ;; 					       'modification-hooks))))
+	    ;; 	;; first line
+	    ;; 	(org--add-to-list-text-property (org-element-property :begin element) start
+	    ;; 				     'modification-hooks #'org--unfold-elements-in-region)
+	    ;; 	;; last line
+	    ;; 	(org--add-to-list-text-property (save-excursion (goto-char end) (line-beginning-position)) end
+	    ;; 				     'modification-hooks #'org--unfold-elements-in-region)))
 	    (org-flag-region start end flag spec))
 	  ;; When the block is hidden away, make sure point is left in
 	  ;; a visible part of the buffer.
