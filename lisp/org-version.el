6a1844d271320663742f0944dd19d0b9f99aef82
Ihor Radchenko <yantar92@gmail.com>
2021-01-13 12:05:58 +0800

    Update version

--- lisp/org-version.el
+++ lisp/org-version.el
@@ -11,7 +11,7 @@ Inserted by installing Org mode or when a release is made."
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "release_9.4.4-382-ge1aa10"))
+   (let ((org-git-version "release_9.4.4-386-gf28b2d"))
      org-git-version))
 
 (provide 'org-version)
