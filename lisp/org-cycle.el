eb857ad9fb69336688969933e4aec3a4ffc99c63
Ihor Radchenko <yantar92@gmail.com>
2021-03-13 18:10:17 +0800

    Speed up initial visibility setting

--- lisp/org-cycle.el
+++ lisp/org-cycle.el
@@ -210,9 +210,9 @@ STATE should be one of the symbols listed in the docstring of
       (save-excursion
 	(goto-char beg)
 	(while (re-search-forward org-drawer-regexp end t)
-	  (if (org-fold-get-folding-spec 'drawer)
+	  (if (org-fold-get-folding-spec)
 	      ;; Do not fold already folded drawers.
-              (goto-char (min end (org-fold-next-folding-state-change 'drawer)))
+              (goto-char (min end (org-fold-next-folding-state-change)))
 	    (let ((drawer (org-element-at-point)))
 	      (when (memq (org-element-type drawer) '(drawer property-drawer))
 		(org-fold-hide-drawer-toggle t nil drawer)
