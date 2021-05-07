f1907c870d0b620cd81dde794e67f10354b5f7bb
Ihor Radchenko <yantar92@gmail.com>
2021-05-07 22:01:02 +0800

    Guard org-element-cache value from changing outside

--- lisp/org.el
+++ lisp/org.el
@@ -11491,7 +11491,9 @@ The tags are fontified when FONTIFY is non-nil."
                      (org-element--cache-find (point)))))
     (if (and (eq 'headline (org-element-type cached))
              (eq (point) (org-element-property :post-affiliated cached)))
-        (org-element-property :tags cached)
+        ;; If we do not wrap result into `copy-list', reference would
+        ;; be returned and cache element might be modified directly.
+        (copy-list (org-element-property :tags cached))
       (and (if fontified
                (org-looking-at-fontified org-tag-line-re)
              (looking-at org-tag-line-re))
