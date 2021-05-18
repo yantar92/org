123530f133a08ccd3ad3d0cff86b0ed0731f6263
Ihor Radchenko <yantar92@gmail.com>
2021-05-18 11:22:02 +0800

    Speed up regexp check

--- lisp/org-cycle.el
+++ lisp/org-cycle.el
@@ -471,7 +471,7 @@ Use `\\[org-edit-special]' to edit table.el tables"))
 	 (outline-next-heading))
 	(when (org-invisible-p) (org-fold-heading nil))))
      ((and (or (>= eol eos)
-	       (not (string-match "\\S-" (buffer-substring eol eos))))
+	       (save-excursion (goto-char eol) (skip-chars-forward "\\s-" eos) (= (point) eos)))
 	   (or has-children
 	       (not (setq children-skipped
 			org-cycle-skip-children-state-if-no-children))))
