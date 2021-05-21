yantar92/org

commit 914fe8429e561c2c3da66eba283d86235cde75c8

Fix org-back-to-heading when there is inlinetask before point

lisp/org.el
@@ -19490,13 +19490,22 @@ interactive command with similar behavior."
   (or (org-at-heading-p (not invisible-ok))
       (let (found)
 	(save-excursion
+          ;; At inlinetask end.  Move to bol, so that the following
+          ;; search goes to the beginning of the inlinetask.
+          (when (and (featurep 'org-inlinetask)
+                     (org-inlinetask-end-p))
+            (goto-char (line-beginning-position)))
 	  (while (not found)
 	    (or (re-search-backward (concat "^\\(?:" outline-regexp "\\)")
 				    nil t)
                 (user-error "Before first headline at position %d in buffer %s"
 		            (point) (current-buffer)))
-	    (setq found (and (or invisible-ok (not (org-fold-folded-p)))
-			     (point)))))
+            ;; Skip inlinetask end.
+            (if (and (featurep 'org-inlinetask)
+                     (org-inlinetask-end-p))
+                (org-inlinetask-goto-beginning)
+	      (setq found (and (or invisible-ok (not (org-fold-folded-p)))
+			       (point))))))
 	(goto-char found)
 	found)))
 

commit 115d522df295003f0b6d6bf77211849d7c4c3038

Fix infinite loop introduced in 914fe8429

lisp/org.el
@@ -12320,7 +12320,7 @@ should be considered as undefined (this is the meaning of nil here).
 However, if LITERAL-NIL is set, return the string value \"nil\" instead."
   (move-marker org-entry-property-inherited-from nil)
   (org-with-wide-buffer
-   (let (value)
+   (let (value at-bob-no-heading)
      (catch 'exit
        (if (org-element--cache-active-p)
            (let ((element (org-element-lineage (org-element-at-point) '(headline org-data inlinetask) 'with-self)))
@@ -12360,7 +12360,10 @@ However, if LITERAL-NIL is set, return the string value \"nil\" instead."
                    ;; `org-up-heading-safe' returned nil.  We are at bob.
                    ;; If there is headline there, do not try to fetch
                    ;; its properties.
-                   (not (org-at-heading-p))))
+                   (and (bobp)
+                        (not at-bob-no-heading)
+                        (not (org-at-heading-p))
+                        (setq at-bob-no-heading t))))
 	      (t
 	       (let ((global (org--property-global-or-keyword-value property literal-nil)))
 	         (cond ((not global))