3f3f3662fdc5328d0c1590b43e6e4fb86a370671
Ihor Radchenko <yantar92@gmail.com>
2021-08-28 13:45:15 +0800

    Reduce the number of times we call O(logN) avl-tree-member-p

--- lisp/org-element.el
+++ lisp/org-element.el
@@ -5963,9 +5963,9 @@ request."
 		      ((and parent
 			    (let ((p (org-element-property :parent data)))
 			      (or (not p)
-                                  (not (avl-tree-member-p org-element--cache p))
 				  (< (org-element-property :begin p)
-				     (org-element-property :begin parent)))))
+				     (org-element-property :begin parent))
+                                  (not (avl-tree-member-p org-element--cache p)))))
                        (org-element--cache-log-message "Updating parent in %S\n New parent: %S"
                                                        (org-element--format-element data)
                                                        (org-element--format-element parent))
