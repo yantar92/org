a947a7bf341f11fe894cbe6bbe872826681bbe3e
Ihor Radchenko <yantar92@gmail.com>
2022-04-12 19:48:10 +0800

    test-org-list/indent-item: Do not bind parser-affecting variables late
    * testing/lisp/test-org-list.el (test-org-list/indent-item): Do
    let-binding before org-mode is activated to avoid confusing parser.

--- testing/lisp/test-org-list.el
+++ testing/lisp/test-org-list.el
@@ -274,14 +274,14 @@
    (equal "
 a. Item 1
    - Item 2"
-	  (org-test-with-temp-text "
+          (let ((org-plain-list-ordered-item-terminator t)
+		(org-list-allow-alphabetical t)
+		(org-list-demote-modify-bullet '(("A." . "a.") ("a." . "-"))))
+	    (org-test-with-temp-text "
 a. Item 1
 b. Item 2<point>"
-	    (let ((org-plain-list-ordered-item-terminator t)
-		  (org-list-allow-alphabetical t)
-		  (org-list-demote-modify-bullet '(("A." . "a.") ("a." . "-"))))
-	      (org-indent-item))
-	    (buffer-string))))
+	      (org-indent-item)
+	      (buffer-string)))))
   ;; When a region is selected, indent every item within.
   (should
    (equal "
