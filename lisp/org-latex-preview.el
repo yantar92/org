812dbf239d90120001945fbe928c173584402429
TEC <git@tecosaur.net>
2023-02-26 19:04:34 +0800

    org-latex-preview: Move org-format-latex to compat
    * lisp/org-latex-preview.el (org-latex-preview-create-image,
    org-format-latex): It has become apparent that it is not reasonably
    possible to maintain the behaviour of `org-format-latex' and
    `org-create-formula-image' using the new image, so instead put the
    original implementations in org-compat.
    (org-preview-latex-image-directory): Move and mark as obsolete in
    org-compat as it has not be used.
    
    * lisp/org-compat.el: Introduce the original `org-format-latex' and
    `org-create-formula-image' definitions.  Also move
    `org-preview-latex-image-directory' here and mark as obsolete, since it
    has ended up being completely unused.

--- lisp/org-latex-preview.el
+++ lisp/org-latex-preview.el
@@ -240,16 +240,6 @@ is required."
   :type '(alist :tag "Compiler"
           :value-type (string :type "command")))
 
-(defcustom org-preview-latex-image-directory "ltximg/"
-  "Path to store latex preview images.
-A relative path here creates many directories relative to the
-processed Org files paths.  An absolute path puts all preview
-images at the same place."
-  :group 'org-latex
-  :version "26.1"
-  :package-version '(Org . "9.0")
-  :type 'string)
-
 (defcustom org-latex-preview-persist t
   "Persist produced LaTeX previews across Emacs sessions.
 
@@ -1014,26 +1004,6 @@ protection against placing doubled up overlays."
                     (org-latex-preview-collect-fragments beg (1- ws)))))
         (error "Unknown conversion process %s for previewing LaTeX fragments"
                processing-type)))))
-
-(defun org-format-latex
-    (prefix &optional beg end dir overlays msg forbuffer processing-type)
-  "Replace LaTeX fragments with links to an image.
-
-The function takes care of creating the replacement image.
-
-Only consider fragments between BEG and END when those are
-provided.
-
-When optional argument OVERLAYS is non-nil, display the image on
-top of the fragment instead of replacing it.
-
-PROCESSING-TYPE is the conversion method to use, as a symbol.
-
-Some of the options can be changed using the variable
-`org-latex-preview-options', which see."
-  (if (and overlays forbuffer)
-      (org-latex-preview-fragments processing-type beg end)
-    (org-latex-preview-replace-fragments prefix processing-type dir msg)))
 
 (defun org-latex-preview--place-from-elements (processing-type elements)
   "Preview LaTeX math fragments ELEMENTS using PROCESSING-TYPE."
@@ -2306,100 +2276,6 @@ the horizontal and vertical directions."
       (round (/ (display-pixel-height)
                 (/ (display-mm-height) 25.4)))
     (error "Attempt to calculate the dpi of a non-graphic display")))
-
-(defun org-latex-preview-create-image
-    (string tofile options buffer &optional processing-type)
-  "Create an image from LaTeX source using external processes.
-
-The LaTeX STRING is saved to a temporary LaTeX file, then
-converted to an image file by process PROCESSING-TYPE defined in
-`org-latex-preview-process-alist'.  A nil value defaults to
-`org-latex-preview-default-process'.
-
-The generated image file is eventually moved to TOFILE.
-
-The OPTIONS argument controls the size, foreground color and
-background color of the generated image.
-
-When BUFFER non-nil, this function is used for LaTeX previewing.
-Otherwise, it is used to deal with LaTeX snippets showed in
-a HTML file."
-  (let* ((processing-type (or processing-type
-                              org-latex-preview-default-process))
-         (processing-info
-          (cdr (assq processing-type org-latex-preview-process-alist)))
-         (programs (plist-get processing-info :programs))
-         (error-message (or (plist-get processing-info :message) ""))
-         (image-input-type (plist-get processing-info :image-input-type))
-         (image-output-type (plist-get processing-info :image-output-type))
-         (post-clean (or (plist-get processing-info :post-clean)
-                         '(".dvi" ".xdv" ".pdf" ".tex" ".aux" ".log"
-                           ".svg" ".png" ".jpg" ".jpeg" ".out")))
-         (latex-preamble
-          (or (plist-get processing-info :latex-header)
-              (org-latex-make-preamble
-               (org-export-get-environment (org-export-get-backend 'latex))
-               org-latex-preview-preamble
-               'snippet)))
-         (latex-compiler (plist-get processing-info :latex-compiler))
-         (tmpdir temporary-file-directory)
-         (texfile-base (make-temp-name
-                        (expand-file-name "orgtex" tmpdir)))
-         (texfile (concat texfile-base ".tex"))
-         (scale (* (if buffer 1.4 1.2)
-                   (or (plist-get options (if buffer :scale :html-scale)) 1.0)))
-         (dpi (* scale (if (and buffer (display-graphic-p)) (org-latex-preview--get-display-dpi) 140.0)))
-         (fg (or (plist-get options (if buffer :foreground :html-foreground))
-                 "Black"))
-         (bg (or (plist-get options (if buffer :background :html-background))
-                 "Transparent"))
-         (image-converter
-          (or (and (string= bg "Transparent")
-                   (plist-get processing-info :transparent-image-converter))
-              (plist-get processing-info :image-converter)))
-         (log-buf (get-buffer-create org-latex-preview--latex-log))
-         (resize-mini-windows nil)) ;Fix Emacs flicker when creating image.
-    (dolist (program programs)
-      (org-check-external-command program error-message))
-    (if (eq fg 'default)
-        (setq fg (org-latex-preview--attr-color :foreground))
-      (setq fg (org-latex-preview--format-color fg)))
-    (setq bg (cond
-              ((eq bg 'default) (org-latex-preview--attr-color :background))
-              ((string= bg "Transparent") nil)
-              (t (org-latex-preview--format-color bg))))
-    ;; Remove TeX \par at end of snippet to avoid trailing space.
-    (if (string-suffix-p string "\n")
-        (aset string (1- (length string)) ?%)
-      (setq string (concat string "%")))
-    (with-temp-file texfile
-      (insert latex-preamble)
-      (insert "\n\\begin{document}\n"
-              "\\definecolor{fg}{rgb}{" fg "}%\n"
-              (if bg
-                  (concat "\\definecolor{bg}{rgb}{" bg "}%\n"
-                          "\n\\pagecolor{bg}%\n")
-                "")
-              "\n{\\color{fg}\n"
-              string
-              "\n}\n"
-              "\n\\end{document}\n"))
-    (let* ((err-msg (format "Please adjust `%s' part of \
-`org-latex-preview-process-alist'."
-                            processing-type))
-           (image-input-file
-            (org-compile-file
-             texfile latex-compiler image-input-type err-msg log-buf))
-           (image-output-file
-            (org-compile-file
-             image-input-file image-converter image-output-type err-msg log-buf
-             `((?D . ,(shell-quote-argument (format "%s" dpi)))
-               (?S . ,(shell-quote-argument (format "%s" (/ dpi 140.0))))))))
-      (copy-file image-output-file tofile 'replace)
-      (dolist (e post-clean)
-        (when (file-exists-p (concat texfile-base e))
-          (delete-file (concat texfile-base e))))
-      image-output-file)))
 
 (defun org-latex-preview--attr-color (attr)
   "Return a RGB color for the LaTeX color package."
