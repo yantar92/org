8b905aa718adba7de3dd165473c9f79e3813d893
Ihor Radchenko <yantar92@posteo.net>
2024-01-06 14:17:31 +0100

    Move macro argument parser/interpreter to org-element.el
    * lisp/org-element.el (org-macro-extract-arguments):
    (org-macro-escape-arguments):
    * lisp/org-macro.el (org-macro-extract-arguments):
    (org-macro-escape-arguments): Move to org-element.el.

--- lisp/org-element.el
+++ lisp/org-element.el
@@ -76,8 +76,6 @@
 (require 'org-fold-core)
 
 (declare-function org-escape-code-in-string "org-src" (s))
-(declare-function org-macro-escape-arguments "org-macro" (&rest args))
-(declare-function org-macro-extract-arguments "org-macro" (s))
 (declare-function org-unescape-code-in-string "org-src" (s))
 
 (defvar org-done-keywords)
@@ -4176,6 +4174,47 @@ CONTENTS is the contents of the object, or nil."
 
 
 ;;;; Macro
+
+(defun org-macro-escape-arguments (&rest args)
+  "Build macro's arguments string from ARGS.
+ARGS are strings.  Return value is a string with arguments
+properly escaped and separated with commas.  This is the opposite
+of `org-macro-extract-arguments'."
+  (let ((s ""))
+    (dolist (arg (reverse args) (substring s 1))
+      (setq s
+	    (concat
+	     ","
+	     (replace-regexp-in-string
+	      "\\(\\\\*\\),"
+	      (lambda (m)
+		(concat (make-string (1+ (* 2 (length (match-string 1 m)))) ?\\)
+			","))
+	      ;; If a non-terminal argument ends on backslashes, make
+	      ;; sure to also escape them as they will be followed by
+	      ;; a comma.
+	      (concat arg (and (not (equal s ""))
+			       (string-match "\\\\+\\'" arg)
+			       (match-string 0 arg)))
+	      nil t)
+	     s)))))
+
+(defun org-macro-extract-arguments (s)
+  "Extract macro arguments from string S.
+S is a string containing comma separated values properly escaped.
+Return a list of arguments, as strings.  This is the opposite of
+`org-macro-escape-arguments'."
+  ;; Do not use `org-split-string' since empty strings are
+  ;; meaningful here.
+  (split-string
+   (replace-regexp-in-string
+    "\\(\\\\*\\),"
+    (lambda (str)
+      (let ((len (length (match-string 1 str))))
+	(concat (make-string (/ len 2) ?\\)
+		(if (zerop (mod len 2)) "\000" ","))))
+    s nil t)
+   "\000"))
 
 (defun org-element-macro-parser ()
   "Parse macro at point, if any.
--- lisp/org-macro.el
+++ lisp/org-macro.el
@@ -292,47 +292,6 @@ a definition in TEMPLATES."
 		      (error "Undefined Org macro: %s; aborting"
 			     (org-element-property :key macro))))))))))))
 
-(defun org-macro-escape-arguments (&rest args)
-  "Build macro's arguments string from ARGS.
-ARGS are strings.  Return value is a string with arguments
-properly escaped and separated with commas.  This is the opposite
-of `org-macro-extract-arguments'."
-  (let ((s ""))
-    (dolist (arg (reverse args) (substring s 1))
-      (setq s
-	    (concat
-	     ","
-	     (replace-regexp-in-string
-	      "\\(\\\\*\\),"
-	      (lambda (m)
-		(concat (make-string (1+ (* 2 (length (match-string 1 m)))) ?\\)
-			","))
-	      ;; If a non-terminal argument ends on backslashes, make
-	      ;; sure to also escape them as they will be followed by
-	      ;; a comma.
-	      (concat arg (and (not (equal s ""))
-			       (string-match "\\\\+\\'" arg)
-			       (match-string 0 arg)))
-	      nil t)
-	     s)))))
-
-(defun org-macro-extract-arguments (s)
-  "Extract macro arguments from string S.
-S is a string containing comma separated values properly escaped.
-Return a list of arguments, as strings.  This is the opposite of
-`org-macro-escape-arguments'."
-  ;; Do not use `org-split-string' since empty strings are
-  ;; meaningful here.
-  (split-string
-   (replace-regexp-in-string
-    "\\(\\\\*\\),"
-    (lambda (str)
-      (let ((len (length (match-string 1 str))))
-	(concat (make-string (/ len 2) ?\\)
-		(if (zerop (mod len 2)) "\000" ","))))
-    s nil t)
-   "\000"))
-
 
 ;;; Helper functions and variables for internal macros
 
