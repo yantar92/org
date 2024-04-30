977116b07d34522b3898f5d383238da5b24dcf12
Ihor Radchenko <yantar92@posteo.net>
2024-04-30 11:31:37 +0300

    Split out property lookup into a new library org-property-search
    * lisp/org-property-search.el: New library.
    * lisp/org-property.el (org-find-property):
    (org-find-olp):
    (org-find-exact-headline-in-buffer):
    (org-find-exact-heading-in-directory):
    (org-find-entry-with-id): Move.

---
+++ lisp/org-property-search.el
@@ -0,0 +1,143 @@
+;;; org-property-search.el --- Searching properties in Org buffers -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018-2024 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This library implements API to search headings with specific
+;; properties.
+
+;;; Code:
+
+(require 'org-macs)
+(org-assert-version)
+
+(require 'org-element)
+(require 'org-element-context)
+(require 'org-move)
+(require 'org-property-core)
+
+(defun org-find-property (property &optional value)
+  "Find first entry in buffer that sets PROPERTY.
+
+When optional argument VALUE is non-nil, only consider an entry
+if it contains PROPERTY set to this value.  If PROPERTY should be
+explicitly set to nil, use string \"nil\" for VALUE.
+
+Return position where the entry begins, or nil if there is no
+such entry.  If narrowing is in effect, only search the visible
+part of the buffer."
+  (save-excursion
+    (goto-char (point-min))
+    (let ((case-fold-search t)
+	  (re (org-re-property property nil (not value) value)))
+      (catch 'exit
+	(while (re-search-forward re nil t)
+	  (when (if value (org-at-property-p)
+		  (org-entry-get-with-inheritance property t))
+	    (throw 'exit (progn (org-back-to-heading-or-point-min t)
+				(point)))))))))
+
+(defun org-find-olp (path &optional this-buffer)
+  "Return a marker pointing to the entry at outline path OLP.
+If anything goes wrong, throw an error, and if you need to do
+something based on this error, you can catch it with
+`condition-case'.
+
+If THIS-BUFFER is set, the outline path does not contain a file,
+only headings."
+  (let* ((file (if this-buffer buffer-file-name (pop path)))
+	 (buffer (if this-buffer (current-buffer) (find-file-noselect file)))
+	 (level 1)
+	 (lmin 1)
+	 (lmax 1)
+	 end found flevel)
+    (unless buffer (error "File not found :%s" file))
+    (with-current-buffer buffer
+      (unless (derived-mode-p 'org-mode)
+	(error "Buffer %s needs to be in Org mode" buffer))
+      (org-with-wide-buffer
+       (goto-char (point-min))
+       (dolist (heading path)
+	 (let ((re (format org-complex-heading-regexp-format
+			   (regexp-quote heading)))
+	       (cnt 0))
+	   (while (re-search-forward re end t)
+	     (setq level (- (match-end 1) (match-beginning 1)))
+	     (when (and (>= level lmin) (<= level lmax))
+	       (setq found (match-beginning 0) flevel level cnt (1+ cnt))))
+	   (when (= cnt 0)
+	     (error "Heading not found on level %d: %s" lmax heading))
+	   (when (> cnt 1)
+	     (error "Heading not unique on level %d: %s" lmax heading))
+	   (goto-char found)
+	   (setq lmin (1+ flevel) lmax (+ lmin (if org-odd-levels-only 1 0)))
+	   (setq end (save-excursion (org-end-of-subtree t t)))))
+       (when (org-at-heading-p)
+	 (point-marker))))))
+
+(defun org-find-exact-headline-in-buffer (heading &optional buffer pos-only)
+  "Find node HEADING in BUFFER.
+Return a marker to the heading if it was found, or nil if not.
+If POS-ONLY is set, return just the position instead of a marker.
+
+The heading text must match exact, but it may have a TODO keyword,
+a priority cookie and tags in the standard locations."
+  (with-current-buffer (or buffer (current-buffer))
+    (org-with-wide-buffer
+     (goto-char (point-min))
+     (let (case-fold-search)
+       (when (re-search-forward
+	      (format org-complex-heading-regexp-format
+		      (regexp-quote heading)) nil t)
+	 (if pos-only
+	     (match-beginning 0)
+	   (move-marker (make-marker) (match-beginning 0))))))))
+
+(defun org-find-exact-heading-in-directory (heading &optional dir)
+  "Find Org node headline HEADING in all \".org\" files in directory DIR.
+When the target headline is found, return a marker to this location."
+  (let ((files (directory-files (or dir default-directory)
+				t "\\`[^.#].*\\.org\\'"))
+	visiting m buffer)
+    (catch 'found
+      (dolist (file files)
+        (message "trying %s" file)
+        (setq visiting (org-find-base-buffer-visiting file))
+        (setq buffer (or visiting (find-file-noselect file)))
+        (setq m (org-find-exact-headline-in-buffer
+                 heading buffer))
+        (when (and (not m) (not visiting)) (kill-buffer buffer))
+        (and m (throw 'found m))))))
+
+(defun org-find-entry-with-id (ident)
+  "Locate the entry that contains the ID property with exact value IDENT.
+IDENT can be a string, a symbol or a number, this function will search for
+the string representation of it.
+Return the position where this entry starts, or nil if there is no such entry."
+  (interactive "sID: ")
+  (let ((id (cond
+	     ((stringp ident) ident)
+	     ((symbolp ident) (symbol-name ident))
+	     ((numberp ident) (number-to-string ident))
+	     (t (error "IDENT %s must be a string, symbol or number" ident)))))
+    (org-with-wide-buffer (org-find-property "ID" id))))
+
+(provide 'org-property-search)
+
+;;; org-property-search.el ends here
--- lisp/org-property.el
+++ lisp/org-property.el
@@ -36,6 +36,7 @@
 (require 'org-outline)
 (require 'org-tags-core)
 (require 'org-property-core)
+(require 'org-property-search)
 (require 'org-mode-common)
 (require 'org-macro)
 (require 'org-planning)
@@ -858,112 +859,6 @@ Throw an error when trying to set a property with an invalid name."
       (when fn (setq value (funcall fn value)))
       (unless (equal (org-entry-get nil property) value)
 	(org-entry-put nil property value)))))
-
-(defun org-find-property (property &optional value)
-  "Find first entry in buffer that sets PROPERTY.
-
-When optional argument VALUE is non-nil, only consider an entry
-if it contains PROPERTY set to this value.  If PROPERTY should be
-explicitly set to nil, use string \"nil\" for VALUE.
-
-Return position where the entry begins, or nil if there is no
-such entry.  If narrowing is in effect, only search the visible
-part of the buffer."
-  (save-excursion
-    (goto-char (point-min))
-    (let ((case-fold-search t)
-	  (re (org-re-property property nil (not value) value)))
-      (catch 'exit
-	(while (re-search-forward re nil t)
-	  (when (if value (org-at-property-p)
-		  (org-entry-get (point) property nil t))
-	    (throw 'exit (progn (org-back-to-heading-or-point-min t)
-				(point)))))))))
-
-(defun org-find-olp (path &optional this-buffer)
-  "Return a marker pointing to the entry at outline path OLP.
-If anything goes wrong, throw an error, and if you need to do
-something based on this error, you can catch it with
-`condition-case'.
-
-If THIS-BUFFER is set, the outline path does not contain a file,
-only headings."
-  (let* ((file (if this-buffer buffer-file-name (pop path)))
-	 (buffer (if this-buffer (current-buffer) (find-file-noselect file)))
-	 (level 1)
-	 (lmin 1)
-	 (lmax 1)
-	 end found flevel)
-    (unless buffer (error "File not found :%s" file))
-    (with-current-buffer buffer
-      (unless (derived-mode-p 'org-mode)
-	(error "Buffer %s needs to be in Org mode" buffer))
-      (org-with-wide-buffer
-       (goto-char (point-min))
-       (dolist (heading path)
-	 (let ((re (format org-complex-heading-regexp-format
-			   (regexp-quote heading)))
-	       (cnt 0))
-	   (while (re-search-forward re end t)
-	     (setq level (- (match-end 1) (match-beginning 1)))
-	     (when (and (>= level lmin) (<= level lmax))
-	       (setq found (match-beginning 0) flevel level cnt (1+ cnt))))
-	   (when (= cnt 0)
-	     (error "Heading not found on level %d: %s" lmax heading))
-	   (when (> cnt 1)
-	     (error "Heading not unique on level %d: %s" lmax heading))
-	   (goto-char found)
-	   (setq lmin (1+ flevel) lmax (+ lmin (if org-odd-levels-only 1 0)))
-	   (setq end (save-excursion (org-end-of-subtree t t)))))
-       (when (org-at-heading-p)
-	 (point-marker))))))
-
-(defun org-find-exact-headline-in-buffer (heading &optional buffer pos-only)
-  "Find node HEADING in BUFFER.
-Return a marker to the heading if it was found, or nil if not.
-If POS-ONLY is set, return just the position instead of a marker.
-
-The heading text must match exact, but it may have a TODO keyword,
-a priority cookie and tags in the standard locations."
-  (with-current-buffer (or buffer (current-buffer))
-    (org-with-wide-buffer
-     (goto-char (point-min))
-     (let (case-fold-search)
-       (when (re-search-forward
-	      (format org-complex-heading-regexp-format
-		      (regexp-quote heading)) nil t)
-	 (if pos-only
-	     (match-beginning 0)
-	   (move-marker (make-marker) (match-beginning 0))))))))
-
-(defun org-find-exact-heading-in-directory (heading &optional dir)
-  "Find Org node headline HEADING in all \".org\" files in directory DIR.
-When the target headline is found, return a marker to this location."
-  (let ((files (directory-files (or dir default-directory)
-				t "\\`[^.#].*\\.org\\'"))
-	visiting m buffer)
-    (catch 'found
-      (dolist (file files)
-        (message "trying %s" file)
-        (setq visiting (org-find-base-buffer-visiting file))
-        (setq buffer (or visiting (find-file-noselect file)))
-        (setq m (org-find-exact-headline-in-buffer
-                 heading buffer))
-        (when (and (not m) (not visiting)) (kill-buffer buffer))
-        (and m (throw 'found m))))))
-
-(defun org-find-entry-with-id (ident)
-  "Locate the entry that contains the ID property with exact value IDENT.
-IDENT can be a string, a symbol or a number, this function will search for
-the string representation of it.
-Return the position where this entry starts, or nil if there is no such entry."
-  (interactive "sID: ")
-  (let ((id (cond
-	     ((stringp ident) ident)
-	     ((symbolp ident) (symbol-name ident))
-	     ((numberp ident) (number-to-string ident))
-	     (t (error "IDENT %s must be a string, symbol or number" ident)))))
-    (org-with-wide-buffer (org-find-property "ID" id))))
 
 (defun org-delete-property (property)
   "In the current entry, delete PROPERTY."
