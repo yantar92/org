VitalyAnkh/org-mode

org-latex-preview: Use prefix more consistently
* lisp/ox-odt.el (org-odt--translate-latex-fragments): Use renamed
org-latex-preview-* functions/variables.

* lisp/ox-html.el (org-html-latex-fragment, org-html-latex-environment,
org-html-format-latex, org-html-with-latex): Use renamed
org-latex-preview-* functions/variables.

* lisp/org.el (org-compute-latex-and-related-regexp,
org-latex-packages-alist): Use renamed org-latex-preview-*
functions/variables.

* lisp/org-latex-preview.el (org-dvipng-color, org-create-formula-image,
org--get-display-dpi, org-create-latex-export,
org-preview-latex--tex-styled, org-preview-precompile,
org-latex-preview--place-images, org-latex-preview--generic-callback,
org-latex-preview--image-extract-async,
org-preview-latex--create-tex-file, org-create-formula-image-async,
org-create-latex-preview, org-format-latex, org-latex-preview-fragments,
org-latex-replace-fragments, org-latex-preview,
org--latex-preview-region, org-clear-latex-preview,
org-latex-preview--handle-insert,
org-latex-preview--close-previous-overlay,
org-latex-preview--handle-post-cursor, org--make-preview-overlay,
org-latex-preview-auto-generate-p, org-preview-use-precompilation,
org-format-latex-header, org-preview-latex-process-alist,
org-preview-latex-default-process, org-format-latex-signal-error,
org-format-latex-options): Use renamed org-latex-preview-*
functions/variables.  Use the org-latex-preview group instead of
org-latex for preview-related variables.

* lisp/org-compat.el: Add entries for renamed and obsoleted
org-latex-preview-* functions/variables.

* lisp/ob-latex.el (org-babel-execute:latex, org-format-latex-header)):
Use renamed org-latex-preview-* functions/variables.

* doc/org-manual.org (Footnotes, Previewing LaTeX fragments, LaTeX
fragments): Use renamed org-latex-preview-* functions/variables.

org-latex-preview: clear overlays name

* lisp/org-latex-preview.el (org-clear-latex-preview):

lisp/org-compat.el
@@ -569,6 +569,47 @@ Counting starts at 1."
                "use `org-element' library"
                "9.0")
 
+(define-obsolete-variable-alias
+  'org-format-latex-options 'org-latex-preview-options "9.7")
+(make-obsolete-variable
+ 'org-format-latex-signal-error "no longer used" "9.7")
+(define-obsolete-variable-alias
+  'org-format-latex-header 'org-latex-preview-header "9.7")
+(define-obsolete-variable-alias
+  'org-preview-latex-default-process 'org-latex-preview-default-process "9.7")
+(define-obsolete-variable-alias
+  'org-preview-latex-process-alist 'org-latex-preview-process-alist "9.7")
+;; (make-obsolete-variable
+;;  'org-preview-latex-image-directory "replaced with org-persist" "9.7")
+(define-obsolete-function-alias
+  'org-clear-latex-preview 'org-latex-preview-clear-overlays "9.7")
+(make-obsolete
+ 'org-place-formula-image "replaced with org-latex-preview-place-image and org-latex-preview-place-image-link" "9.7")
+(define-obsolete-function-alias
+  'org-create-formula-image 'org-latex-preview-create-image "9.7")
+(define-obsolete-function-alias
+  'org-latex-color-format 'org-latex-preview--format-color "9.7")
+(define-obsolete-function-alias
+  'org-latex-color 'org-latex-preview--attr-color "9.7")
+(define-obsolete-function-alias
+  'org-normalize-color 'org-latex-preview--normalize-color "9.7")
+
+;; FIXME: Unused; obsoleted; to be removed.
+(defun org-dvipng-color (attr)
+  "Return a RGB color specification for dvipng."
+  (org-dvipng-color-format (face-attribute 'default attr nil)))
+
+;; FIXME: Unused; obsoleted; to be removed.
+(defun org-dvipng-color-format (color-name)
+  "Convert COLOR-NAME to a RGB color value for dvipng."
+  (apply #'format "rgb %s %s %s"
+         (mapcar 'org-latex-preview--normalize-color
+                 (color-values color-name))))
+(make-obsolete
+ 'org-dvipng-color "to be removed" "9.7")
+(make-obsolete
+ 'org-dvipng-color-format "to be removed" "9.7")
+
 ;; FIXME: Unused; obsoleted; to be removed.
 (defun org-let (list &rest body) ;FIXME: So many kittens are suffering here.
   (declare (indent 1) (obsolete cl-progv "2021"))

yantar92/org

lisp/org-compat.el
@@ -720,6 +720,47 @@ This constant, for example, makes the below code not err:
                "use `org-element' library"
                "9.0")
 
+(define-obsolete-variable-alias
+  'org-format-latex-options 'org-latex-preview-options "9.7")
+(make-obsolete-variable
+ 'org-format-latex-signal-error "no longer used" "9.7")
+(define-obsolete-variable-alias
+  'org-format-latex-header 'org-latex-preview-preamble "9.7")
+(define-obsolete-variable-alias
+  'org-preview-latex-default-process 'org-latex-preview-default-process "9.7")
+(define-obsolete-variable-alias
+  'org-preview-latex-process-alist 'org-latex-preview-process-alist "9.7")
+;; (make-obsolete-variable
+;;  'org-preview-latex-image-directory "replaced with org-persist" "9.7")
+(define-obsolete-function-alias
+  'org-clear-latex-preview 'org-latex-preview-clear-overlays "9.7")
+(make-obsolete
+ 'org-place-formula-image "replaced with org-latex-preview-place-image and org-latex-preview-place-image-link" "9.7")
+(define-obsolete-function-alias
+  'org-create-formula-image 'org-latex-preview-create-image "9.7")
+(define-obsolete-function-alias
+  'org-latex-color-format 'org-latex-preview--format-color "9.7")
+(define-obsolete-function-alias
+  'org-latex-color 'org-latex-preview--attr-color "9.7")
+(define-obsolete-function-alias
+  'org-normalize-color 'org-latex-preview--normalize-color "9.7")
+
+;; FIXME: Unused; obsoleted; to be removed.
+(defun org-dvipng-color (attr)
+  "Return a RGB color specification for dvipng."
+  (org-dvipng-color-format (face-attribute 'default attr nil)))
+
+;; FIXME: Unused; obsoleted; to be removed.
+(defun org-dvipng-color-format (color-name)
+  "Convert COLOR-NAME to a RGB color value for dvipng."
+  (apply #'format "rgb %s %s %s"
+         (mapcar 'org-latex-preview--normalize-color
+                 (color-values color-name))))
+(make-obsolete
+ 'org-dvipng-color "to be removed" "9.7")
+(make-obsolete
+ 'org-dvipng-color-format "to be removed" "9.7")
+
 ;; FIXME: Unused; obsoleted; to be removed.
 (defun org-let (list &rest body) ;FIXME: So many kittens are suffering here.
   (declare (indent 1) (obsolete cl-progv "2021"))