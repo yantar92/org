yantar92/org

commit 67922b8c0319f30c5e0192315a1a677702aba852

org-agenda-prefix-format: Clarify where %(expression) is evaluated
* lisp/org-agenda.el (org-agenda-prefix-format): Document that
expression is evaluated in the agenda buffer, not in Org buffer.

Reported-by: lou <lou@repetitions.de>
Link: https://orgmode.org/list/87wm2wzws0.fsf@repetitions.de

lisp/org-agenda.el
@@ -1761,7 +1761,10 @@ meaning:
   %s   Scheduling/Deadline information, a short string
   %b   show breadcrumbs, i.e., the names of the higher levels
   %(expression) Eval EXPRESSION and replace the control string
-                by the result
+                by the result.  EXPRESSION is evaluated with point
+                in agenda buffer, not in the Org buffer.  Use
+                `org-agenda-with-point-at-orig-entry' if you need to
+                move point to entry's position in Org buffer.
 
 All specifiers work basically like the standard `%s' of `format', but
 may contain two additional characters: a question mark just after the

commit 85ed5be60f38335a035e2e8752e7280a6e704cd4

org-agenda-prefix-format: Describe accurately where format is evaluated
* lisp/org-agenda.el (org-agenda-prefix-format): Fix incorrect
explanation introduced in 67922b8c0.  Rather than being evaluated in
agenda buffer all the time, the behavior is more subtle.

Link: https://orgmode.org/list/87pl8em2wc.fsf@repetitions.de

lisp/org-agenda.el
@@ -1761,10 +1761,14 @@ meaning:
   %s   Scheduling/Deadline information, a short string
   %b   show breadcrumbs, i.e., the names of the higher levels
   %(expression) Eval EXPRESSION and replace the control string
-                by the result.  EXPRESSION is evaluated with point
-                in agenda buffer, not in the Org buffer.  Use
-                `org-agenda-with-point-at-orig-entry' if you need to
-                move point to entry's position in Org buffer.
+                by the result.
+
+EXPRESSION is evaluated with point and buffer associated with agenda
+entry/line being rendered.  For example, heading lines will be
+evaluated with point in the Org buffer at that corresponding heading.
+However, diary records will not be evaluated in an Org buffer, but
+inside diary buffer.  Auxiliary agenda lines like time grids will be
+evaluated with the point inside agenda buffer itself.
 
 All specifiers work basically like the standard `%s' of `format', but
 may contain two additional characters: a question mark just after the